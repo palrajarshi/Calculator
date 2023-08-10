f80db789f487768e4d3393cfb91616e49a9404d2
Rajarshi <rajarship23@gmail.com>
2023-08-10 13:20:42 +0530

    Intial Commit

---
+++ index1.js
@@ -0,0 +1,40 @@
+const buttons = document.querySelectorAll(".btn");
+let inputDisp = document.getElementById("disp");
+
+// Click event listener for buttons
+buttons.forEach((element, index) => {
+  buttons[index].addEventListener("click", () => {
+    const buttonVal = buttons[index].textContent;
+    console.log(buttonVal);
+    if (buttonVal === "=") {
+      evaluate();
+    } else if (buttonVal === "C") {
+      clear();
+    } else {
+      show(buttonVal);
+    }
+  });
+});
+// Adds the value logged into the display
+const show = (buttonVal) => {
+  console.log("val is", buttonVal);
+  inputDisp.value += buttonVal;
+};
+// Evaluation of result
+const evaluate = () => {
+  if (inputDisp.value.includes("x")) {
+    inputDisp.value = inputDisp.value.replace(/x/g, "*");
+  }
+  const res = eval(inputDisp.value);
+  inputDisp.value = res;
+};
+// Clear Button Logic
+const clear = () => {
+  inputDisp.value = "";
+};
+
+// Cancel Button Logic
+const cancel = document.querySelector(".btn-cancel");
+cancel.addEventListener("click", () => {
+  inputDisp.value = inputDisp.value.substring(0, inputDisp.value.length - 1);
+});
